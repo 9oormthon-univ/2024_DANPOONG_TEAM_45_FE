1624c828523666a4c9bd9f218e92ff9fade2a60d
jiwon <pjw99032986@gmail.com>
2024-11-18 15:41:46 +0900

    FEAT(BASE) : base abstract class
    Base 추상 클래스 세팅

---
+++ app/src/main/java/com/example/myapplication/presentation/base/BaseDiffCallback.kt
@@ -0,0 +1,15 @@
+package com.example.codingland.presenter.base
+
+import androidx.recyclerview.widget.DiffUtil
+
+class BaseDiffCallback<T : Any>(
+    private val itemsTheSame: (oldItem: T, newItem: T) -> Boolean,
+    private val contentsTheSame: (oldItem: T, newItem: T) -> Boolean
+) : DiffUtil.ItemCallback<T>() {
+    override fun areItemsTheSame(oldItem: T, newItem: T): Boolean {
+        return itemsTheSame(oldItem, newItem)
+    }
+    override fun areContentsTheSame(oldItem: T, newItem: T): Boolean {
+        return contentsTheSame(oldItem, newItem)
+    }
+}
