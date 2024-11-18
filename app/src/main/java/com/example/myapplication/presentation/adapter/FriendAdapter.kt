adbf3c32fe7b0465f6d22b0e4589fac06b136310
jiwon <pjw99032986@gmail.com>
2024-11-19 00:12:47 +0900

    FEAT(FriendAdapter.kt) : 친구 랭킹 리사이클러뷰 어댑터
    친구 랭킹을 보여주는 리사이클러 뷰 어댑터

---
+++ app/src/main/java/com/example/myapplication/presentation/adapter/FriendAdapter.kt
@@ -0,0 +1,29 @@
+package com.example.myapplication.presentation.adapter
+
+import com.example.codingland.presenter.base.BaseAdapter
+import com.example.codingland.presenter.base.BaseDiffCallback
+import com.example.myapplication.R
+import com.example.myapplication.databinding.ItemFriendBinding
+import com.example.myapplication.domain.model.FriendsEntity
+import com.example.myapplication.presentation.widget.extention.loadProfileImage
+
+
+class FriendAdapter(
+) : BaseAdapter<FriendsEntity, ItemFriendBinding>(
+    BaseDiffCallback(
+        itemsTheSame = { oldItem, newItem -> oldItem == newItem },
+        contentsTheSame = { oldItem, newItem -> oldItem == newItem }
+    )
+) {
+    override val layoutId: Int
+        get() = R.layout.item_friend
+
+    override fun bind(binding: ItemFriendBinding, item: FriendsEntity) {
+        with(binding) {
+            friendData = item
+            itemFriendIv.loadProfileImage(item.profile)
+        }
+    }
+
+
+}
