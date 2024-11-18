75ee895f40ed107a0a0a674b0b96c4ef52605aaf
jiwon <pjw99032986@gmail.com>
2024-11-18 17:39:08 +0900

    FEAT(LoginFragment.kt) : 로그인 모듈 세팅
    로그인 모듈 세팅

--- app/src/main/java/com/example/myapplication/data/di/ApiModule.kt
+++ app/src/main/java/com/example/myapplication/data/di/ApiModule.kt
@@ -1,11 +1,20 @@
 package com.example.myapplication.data.di
 
+import com.example.myapplication.data.repository.remote.api.LoginApi
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object ApiModule {
+    @Provides
+    @Singleton
+    fun provideLogInServer(
+        @MainRetrofit retrofit: Retrofit
+    ): LoginApi = retrofit.create(LoginApi::class.java)
 
 }
--- app/src/main/java/com/example/myapplication/data/di/RepositoryModule.kt
+++ app/src/main/java/com/example/myapplication/data/di/RepositoryModule.kt
@@ -1,12 +1,30 @@
 package com.example.myapplication.data.di
 
+import com.example.myapplication.data.repository.remote.api.LoginApi
+import com.example.myapplication.data.repository.remote.datasource.remote.LogInDataSource
+import com.example.myapplication.data.repository.remote.datasourceImpl.LogInDataSourceImpl
+import com.example.myapplication.domain.repository.login.LoginRepository
+import com.example.myapplication.domain.repository.login.LoginRepositoryImpl
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object RepositoryModule {
 
+    @Provides
+    @Singleton
+    fun provideLogInDataSource(
+        loginApi: LoginApi
+    ): LogInDataSource {
+        return LogInDataSourceImpl(loginApi)
+    }
 
+    @Singleton
+    @Provides
+    fun provideAccountRepository(logInDataSource: LogInDataSource): LoginRepository =
+        LoginRepositoryImpl(logInDataSource)
 }
