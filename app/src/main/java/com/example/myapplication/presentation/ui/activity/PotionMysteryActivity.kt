d58e05723b4ef940a82e686b6da02778e89fd615
jiwon <pjw99032986@gmail.com>
2024-11-19 00:33:08 +0900

    FEAT(PotionMysteryActivity.kt) : 물약 획득 액티비티
    미스테리 포션 획득 액티비티 기능 코딩

---
+++ app/src/main/java/com/example/myapplication/presentation/ui/activity/PotionMysteryActivity.kt
@@ -0,0 +1,13 @@
+package com.example.myapplication.presentation.ui.activity
+
+import com.example.codingland.presenter.base.BaseActivity
+import com.example.myapplication.R
+import com.example.myapplication.databinding.ActivityPotionMysteryBinding
+
+class PotionMysteryActivity :
+    BaseActivity<ActivityPotionMysteryBinding>(R.layout.activity_potion_mystery) {
+
+    override fun setLayout() {
+
+    }
+}
