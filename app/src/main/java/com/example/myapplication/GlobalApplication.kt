b27acc852d5148b5d52746f7db4e15311b8b8fe7
jiwon <pjw99032986@gmail.com>
2024-11-18 16:45:40 +0900

    FEAT(GlobalApplication.kt) : Application 클래스
    기본 Application 클래스 정의

---
+++ app/src/main/java/com/example/myapplication/GlobalApplication.kt
@@ -0,0 +1,31 @@
+package com.example.myapplication
+
+import android.app.Application
+import com.example.myapplication.data.di.AppCoroutineScope
+import com.example.myapplication.presentation.widget.extention.TokenManager
+import com.kakao.sdk.common.KakaoSdk
+import dagger.hilt.android.HiltAndroidApp
+import kotlinx.coroutines.CoroutineScope
+import javax.inject.Inject
+
+@HiltAndroidApp
+class GlobalApplication : Application() {
+
+    @Inject
+    lateinit var tokenManager: TokenManager
+
+    @Inject
+    @AppCoroutineScope
+    lateinit var scope: CoroutineScope
+
+    override fun onCreate() {
+        super.onCreate()
+        instance = this
+        KakaoSdk.init(this, resources.getString(R.string.appKey))
+    }
+
+    companion object {
+        lateinit var instance: GlobalApplication
+            private set
+    }
+}
