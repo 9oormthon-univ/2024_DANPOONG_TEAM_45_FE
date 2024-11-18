1523da56d5f277aca1187de5b00a88f77ee109be
jiwon <pjw99032986@gmail.com>
2024-11-18 16:44:10 +0900

    FEAT(NETWORK) : 기본 메인 엑티비티 수정
    기본메인 엑티비티 삭제 및 이동

---
+++ app/src/main/java/com/example/myapplication/presentation/ui/activity/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.myapplication.presentation.ui.activity
+
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.example.codingland.presenter.base.BaseActivity
+import com.example.myapplication.R
+import com.example.myapplication.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
+    private lateinit var navController: NavController
+
+    override fun setLayout() {
+        setBottomNavigation()
+    }
+
+    //바텀 네비게이션 세팅
+    private fun setBottomNavigation() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(binding.activityMainFcv.id) as NavHostFragment
+        navController = navHostFragment.navController
+        binding.activityMainBnv.setupWithNavController(navController)
+    }
+
+}
