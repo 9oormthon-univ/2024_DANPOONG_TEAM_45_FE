c10aaf74941d136545ac5a067b6c55ffff17dfeb
jiwon <pjw99032986@gmail.com>
2024-11-18 16:43:46 +0900

    FEAT(NETWORK) : 네트워크 기초 세팅
    네트워크 모듈 기초 세팅

---
+++ app/src/main/java/com/example/myapplication/data/di/NetWorkModule.kt
@@ -0,0 +1,70 @@
+package com.example.myapplication.data.di
+
+import com.example.myapplication.presentation.widget.extention.AccessTokenInterceptor
+import com.example.myapplication.presentation.widget.extention.TokenManager
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Named
+import javax.inject.Singleton
+
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Singleton
+    @Provides
+    fun provideHttpLoggingInterceptor(): HttpLoggingInterceptor {
+        return HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+            level = HttpLoggingInterceptor.Level.HEADERS
+        }
+    }
+
+    @Singleton
+    @Provides
+    fun provideAccessTokenInterceptor(tokenManager: TokenManager): AccessTokenInterceptor {
+        return AccessTokenInterceptor(tokenManager)
+    }
+
+    @Provides
+    @Singleton
+    fun provideConverterFactory(): GsonConverterFactory = GsonConverterFactory.create()
+
+    @Singleton
+    @Provides
+    @Named("defaultOkHttpClient")
+    fun provideOkHttpClient(
+        httpLoggingInterceptor: HttpLoggingInterceptor,
+        accessTokenInterceptor: AccessTokenInterceptor,
+    ): OkHttpClient {
+        return OkHttpClient.Builder()
+            .connectTimeout(30, TimeUnit.SECONDS)
+            .readTimeout(30, TimeUnit.SECONDS)
+            .writeTimeout(30, TimeUnit.SECONDS)
+            .addInterceptor(httpLoggingInterceptor)
+            .addInterceptor(accessTokenInterceptor)
+            .build()
+    }
+
+    @MainRetrofit
+    @Singleton
+    @Provides
+    fun provideMainRetrofit(
+        @Named("defaultOkHttpClient") okHttpClient: OkHttpClient,
+        gsonConverterFactory: GsonConverterFactory
+    ): Retrofit {
+        return Retrofit.Builder()
+            .addConverterFactory(gsonConverterFactory)
+            .client(okHttpClient)
+            .baseUrl("https://codingland.link")
+            .build()
+    }
+}
