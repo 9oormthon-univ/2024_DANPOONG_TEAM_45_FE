1db07888f71243d11a25599e56eccac635f2072a
jiwon <pjw99032986@gmail.com>
2024-11-19 16:10:59 +0900

    FEAT(chapterApi) : 챕터 관련 API
    챕터 관련 API
    데이터 소스, 유스 케이스, 레포지토리, 서비스 , 구현체 작성

---
+++ app/src/main/java/com/example/myapplication/domain/repository/chaptercleared/ChapterClearedRepositoryImpl.kt
@@ -0,0 +1,33 @@
+package com.example.myapplication.domain.repository.chaptercleared
+
+import com.example.codingland.data.repository.remote.datasource.remote.IsChapterClearedDataSource
+import com.example.codingland.data.repository.remote.response.chaptercleared.ClearChapterStateListResponse
+import com.example.codingland.data.repository.remote.response.chaptercleared.ClearChapterStateResponse
+import com.example.codingland.domain.repository.chaptercleared.ChapterClearedRepository
+import kotlinx.coroutines.flow.Flow
+import okhttp3.ResponseBody
+import retrofit2.Response
+import javax.inject.Inject
+
+data class ChapterClearedRepositoryImpl @Inject constructor(
+    private val isChapterClearedDataSource: IsChapterClearedDataSource
+) : ChapterClearedRepository {
+    override suspend fun postChapterCleared(
+        chapterId: Int,
+        user_id: Int
+    ): Flow<Response<ResponseBody>> =
+        isChapterClearedDataSource.postChapterCleared(chapterId,user_id)
+
+    override suspend fun getChapterDistinct(isChapterCleared_id: Int): Flow<Response<ClearChapterStateResponse>> =
+        isChapterClearedDataSource.getChapterDistinct(isChapterCleared_id)
+
+    override suspend fun putChapterCleared(
+        isChapterCleared_id: Int,
+        isCleared: Boolean
+    ): Flow<Response<ResponseBody>> =
+        isChapterClearedDataSource.putChapterCleared(isChapterCleared_id, isCleared)
+
+    override suspend fun getChapterAll(): Flow<Response<ClearChapterStateListResponse>> =
+        isChapterClearedDataSource.getChapterAll()
+
+}
