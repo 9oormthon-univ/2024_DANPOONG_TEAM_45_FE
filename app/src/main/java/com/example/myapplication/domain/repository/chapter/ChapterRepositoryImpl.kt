61283b03037d4b367643a55150e0403df0a30930
jiwon <pjw99032986@gmail.com>
2024-11-19 16:11:52 +0900

    FEAT(chapterApi) : 챕터 관련 API
    챕터 관련 API
    데이터 소스, 유스 케이스, 레포지토리, 서비스 , 구현체 작성

---
+++ app/src/main/java/com/example/myapplication/domain/repository/chapter/ChapterRepositoryImpl.kt
@@ -0,0 +1,36 @@
+package com.example.codingland.domain.repository.chapter
+
+import com.example.myapplication.data.repository.remote.datasource.remote.ChapterDataSource
+import com.example.codingland.data.repository.remote.request.chapter.RegisterChapterDto
+import com.example.codingland.data.repository.remote.response.chapter.AllChapterResponse
+import com.example.codingland.data.repository.remote.response.chapter.DistinctChapterResponse
+import com.example.myapplication.domain.repository.chapter.ChapterRepository
+import kotlinx.coroutines.flow.Flow
+import okhttp3.ResponseBody
+import retrofit2.Response
+import javax.inject.Inject
+
+data class ChapterRepositoryImpl @Inject constructor(
+    private val chapterDataSource: ChapterDataSource
+) : ChapterRepository {
+    override suspend fun postCreateChapter(registerChapterDto: RegisterChapterDto): Flow<Response<ResponseBody>> =
+        chapterDataSource.postCreateChapter(registerChapterDto)
+
+    override suspend fun getDistinctChapter(
+        chapter_id: Int,
+        user_id: Int
+    ): Flow<Response<DistinctChapterResponse>> =
+        chapterDataSource.getDistinctChapter(
+            chapter_id,
+            user_id
+        )
+
+    override suspend fun deleteChapter(lchapter_id: String): Flow<Response<ResponseBody>> =
+        chapterDataSource.deleteChapter(lchapter_id)
+
+    override suspend fun modifyChapter(registerChapterDto: RegisterChapterDto): Flow<Response<ResponseBody>> =
+        chapterDataSource.modifyChapter(registerChapterDto)
+
+    override suspend fun getAllChapter(): Flow<Response<AllChapterResponse>> =
+        chapterDataSource.getAllChapter()
+}
