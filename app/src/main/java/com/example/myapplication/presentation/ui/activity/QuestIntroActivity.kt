e339b570633df76447166b20711f3eef1e4629c2
Eunchae Cho <choeunchae0828@gmail.com>
2024-11-19 20:39:04 +0900

    FEAT(QuestIntroActivity, QuizClearActivity) : UI 추가

--- app/src/main/java/com/example/myapplication/presentation/ui/activity/QuestIntroActivity.kt
+++ app/src/main/java/com/example/myapplication/presentation/ui/activity/QuestIntroActivity.kt
@@ -13,12 +13,20 @@ class QuestIntroActivity :
     override fun setLayout() {
 
         val islandName = intent.getStringExtra("island name")
+        val gameId = intent.getIntExtra("game id", -1)
 
         when (islandName) {
             resources.getString(R.string.biginner_island) -> {
-                binding.tvQuestIntro.text = "코딩 모험의 준비를\n위한 섬에 도착했어요!"
-                binding.ivQuestIntro.loadCropImage(R.drawable.iv_biginner_island)
-                binding.tvQuestIntroSub.text = "먼 길을 떠나기 전, 초심자의 섬 곳곳에 흩어진 \n미션들을 해결하며 기초를 쌓아 볼까요?"
+                if (gameId == 1){
+                    binding.tvQuestIntro.text = "코딩 모험의 준비를\n위한 섬에 도착했어요!"
+                    binding.ivQuestIntro.loadCropImage(R.drawable.iv_biginner_island)
+                    binding.tvQuestIntroSub.text = "먼 길을 떠나기 전, 초심자의 섬 곳곳에 흩어진 \n미션들을 해결하며 기초를 쌓아 볼까요?"
+                }
+                else {
+                    binding.tvQuestIntro.text = "이제 본격적인\n모험 준비를 해볼까요?"
+                    binding.ivQuestIntro.loadCropImage(R.drawable.iv_biginner_island)
+                    binding.tvQuestIntroSub.text = "초심자의 섬의 퀘스트를 모두 통과하면\n무무는 더 강해질 수 있어요"
+                }
             }
             else -> {
                 binding.tvQuestIntro.text = "달콤한 향기가\n가득한 섬에 도착했어요!"
