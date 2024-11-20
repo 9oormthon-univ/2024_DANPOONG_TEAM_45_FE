9f0bb2cb07c4cae382f0fdf1d6d96d20e40d69cd
jiwon <pjw99032986@gmail.com>
2024-11-21 04:13:42 +0900

    FEAT(HomeApi) : 홈 Api 추가
    Home Api 추가

--- app/src/main/java/com/example/myapplication/domain/usecase/home/GetAllHomeUseCase.kt
+++ app/src/main/java/com/example/myapplication/domain/usecase/home/GetAllHomeUseCase.kt
@@ -1,4 +1,14 @@
 package com.example.myapplication.domain.usecase.home
 
-class GetAllHomeUseCase {
+import com.example.myapplication.data.repository.remote.request.home.PatchHomeDTO
+import com.example.myapplication.data.repository.remote.response.BaseResponse
+import com.example.myapplication.data.repository.remote.response.home.HomeAllList
+import com.example.myapplication.domain.repository.home.HomeRepository
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class GetAllHomeUseCase @Inject constructor(
+    private val homeRepository: HomeRepository
+) {
+    suspend operator fun invoke():Flow<HomeAllList> = homeRepository.getAllHome()
 }
