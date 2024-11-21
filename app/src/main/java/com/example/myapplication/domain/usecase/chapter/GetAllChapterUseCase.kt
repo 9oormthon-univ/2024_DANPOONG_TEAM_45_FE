1c73436def3c4ed4a67b062316326d87c0ea028e
jiwon <pjw99032986@gmail.com>
2024-11-22 03:51:16 +0900

    REFACTOR(Chapter) : 챕터 api 수정
    챕터 api 수정

--- app/src/main/java/com/example/myapplication/domain/usecase/chapter/GetAllChapterUseCase.kt
+++ app/src/main/java/com/example/myapplication/domain/usecase/chapter/GetAllChapterUseCase.kt
@@ -1,5 +1,6 @@
 package com.example.myapplication.domain.usecase.chapter
 
+import com.example.myapplication.data.repository.remote.response.BaseResponse
 import com.example.myapplication.data.repository.remote.response.chapter.AllChapterResponse
 import com.example.myapplication.domain.repository.chapter.ChapterRepository
 import kotlinx.coroutines.flow.Flow
@@ -10,5 +11,5 @@ class GetAllChapterUseCase @Inject constructor(
     private val chapterRepository: ChapterRepository
 ) {
     suspend operator fun invoke(
-    ): Flow<Response<AllChapterResponse>> = chapterRepository.getAllChapter()
+    ): Flow<BaseResponse<AllChapterResponse>> = chapterRepository.getAllChapter()
 }
