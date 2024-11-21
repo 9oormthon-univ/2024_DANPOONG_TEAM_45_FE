c0855b3614c8bd77abc318ae758901880833ef95
jiwon <pjw99032986@gmail.com>
2024-11-21 22:59:47 +0900

    REFACTOR(CHARACTER) : 캐릭터 api
    캐릭터 api 수정
    - 캐릭터 api 응답 값 서버 값에 맞추어 변경

--- app/src/main/java/com/example/myapplication/data/repository/remote/api/CharacterApi.kt
+++ app/src/main/java/com/example/myapplication/data/repository/remote/api/CharacterApi.kt
@@ -1,47 +1,63 @@
 package com.example.myapplication.data.repository.remote.api
 
 import com.example.myapplication.data.repository.remote.request.character.CharacterDTO
-import com.example.myapplication.data.repository.remote.request.home.PatchHomeDTO
+import com.example.myapplication.data.repository.remote.response.BaseResponse
 import com.example.myapplication.data.repository.remote.response.character.CharacterAllResponse
 import com.example.myapplication.data.repository.remote.response.character.CharacterResponse
-import com.example.myapplication.data.repository.remote.response.home.DistinctHomeIdResponse
+import com.example.myapplication.data.repository.remote.response.character.CommitCharacterResponse
 import okhttp3.ResponseBody
 import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.DELETE
 import retrofit2.http.GET
-import retrofit2.http.PATCH
 import retrofit2.http.POST
+import retrofit2.http.PUT
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface CharacterApi {
 
+    //캐릭터 이름 수정
+    @PUT("/v1/api/character/{character_id}")
+    suspend fun putCharacter(
+        @Path("character_id") characterId : Int,
+        @Query("name") name: String
+    ): BaseResponse<Any>
+
     //캐릭터 등록
-    @POST("/character")
+    @POST("/v1/api/character")
     suspend fun postCharacter(
         @Body characterDTO: CharacterDTO
-    ) : Response<ResponseBody>
+    ): BaseResponse<CommitCharacterResponse>
+
+    //활동 포인트 증가
+    @POST("/v1/api/character/increaseActivityPoint")
+    suspend fun postIncreaseActivityPoint(
+        @Query("character_id") character_id : Int,
+        @Query("activityPoint") activityPoint : Int
+    ): BaseResponse<Any>
+
+    //활동 포인트 감소
+    @POST("/v1/api/character/decreaseActivityPoint")
+    suspend fun postDecreaseActivityPoint(
+        @Query("character_id") character_id : Int,
+        @Query("activityPoint") activityPoint : Int
+    ): BaseResponse<Any>
 
     //캐릭터 단 건 조회
     @GET("/character")
     suspend fun getCharacter(
-        @Path("id") id : String
-    ) : CharacterResponse
+        @Path("id") id: String
+    ): CharacterResponse
 
     //캐릭터 모두 조회
     @GET("/character/all")
     suspend fun getAllCharacter(
-    ) : CharacterAllResponse
-
-    //캐릭터 수정
-    @PATCH("/character")
-    suspend fun patchCharacter(
-        @Body characterDTO: CharacterDTO
-    ) : Response<ResponseBody>
+    ): CharacterAllResponse
 
     //캐릭터 삭제
     @DELETE("/character")
     suspend fun deleteCharacter(
-        @Path("id") id : String
-    ) : Response<ResponseBody>
+        @Path("id") id: String
+    ): Response<ResponseBody>
 }
---
+++ app/src/main/java/com/example/myapplication/data/repository/remote/response/home/DistinctCharacterResponse.kt
@@ -0,0 +1,9 @@
+package com.example.myapplication.data.repository.remote.response.home
+
+data class DistinctCharacterResponse(
+    val id : Int = 0,
+    val name : String = "",
+    val level : Int = 0,
+    val type : String = "",
+    val activityPoints : Int = 0
+)
