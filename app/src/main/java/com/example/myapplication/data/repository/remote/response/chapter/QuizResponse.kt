baee1bb11b452882b2d0ff0b09d61ea263efd3eb
jiwon <pjw99032986@gmail.com>
2024-11-22 03:55:05 +0900

    REFACTOR(Quiz) : 퀴즈 api 추가
    퀴즈 api 추가

--- app/src/main/java/com/example/myapplication/data/repository/remote/response/chapter/QuizResponse.kt
+++ app/src/main/java/com/example/myapplication/data/repository/remote/response/chapter/QuizResponse.kt
@@ -1,9 +1,8 @@
 package com.example.myapplication.data.repository.remote.response.chapter
 
 data class QuizResponse(
-    val isCleared: Boolean,
-    val level: Int,
     val quizId: Int,
     val title: String,
-    val type: String
+    val level: Int,
+    val isCleared: Boolean
 )
