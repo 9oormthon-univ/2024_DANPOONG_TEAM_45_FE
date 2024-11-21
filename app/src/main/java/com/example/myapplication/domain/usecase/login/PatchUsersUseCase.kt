f9b6282727d160243d3372701119158fe8757978
jiwon <pjw99032986@gmail.com>
2024-11-22 03:56:05 +0900

    FEAT(Login) : 로그인 유스케이스
    로그인 유스케이스 추가

---
+++ app/src/main/java/com/example/myapplication/domain/usecase/login/PatchUsersUseCase.kt
@@ -0,0 +1,15 @@
+package com.example.myapplication.domain.usecase.login
+
+import com.example.myapplication.data.repository.remote.request.login.UserDTO
+import com.example.myapplication.data.repository.remote.response.BaseResponse
+import com.example.myapplication.domain.repository.login.LoginRepository
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class PatchUsersUseCase @Inject constructor(
+    private val loginRepository: LoginRepository
+) {
+    suspend operator fun invoke(
+        userDTO: UserDTO
+    ): Flow<BaseResponse<Any>> = loginRepository.patchUsers(userDTO)
+}
