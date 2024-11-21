e2f85fd2ed68ab116bca54acafaf2c481f74b2a2
jiwon <pjw99032986@gmail.com>
2024-11-21 23:00:18 +0900

    REFACTOR(HOME) : 홈 api 수정
    홈 api 수정
    - 홈 api 응답 값 서버 값에 맞추어 변경

--- app/src/main/java/com/example/myapplication/domain/usecase/home/GetDistinctHomeUseCase.kt
+++ app/src/main/java/com/example/myapplication/domain/usecase/home/GetDistinctHomeUseCase.kt
@@ -1,5 +1,6 @@
 package com.example.myapplication.domain.usecase.home
 
+import com.example.myapplication.data.repository.remote.response.BaseResponse
 import com.example.myapplication.data.repository.remote.response.home.DistinctHomeIdResponse
 import com.example.myapplication.data.repository.remote.response.home.HomeAllList
 import com.example.myapplication.domain.repository.home.HomeRepository
@@ -9,5 +10,5 @@ import javax.inject.Inject
 class GetDistinctHomeUseCase @Inject constructor(
     private val homeRepository: HomeRepository
 ) {
-    suspend operator fun invoke(home_id: Int): Flow<DistinctHomeIdResponse> = homeRepository.getDistinctHome(home_id)
+    suspend operator fun invoke(): Flow<BaseResponse<DistinctHomeIdResponse>> = homeRepository.getDistinctHome()
 }
