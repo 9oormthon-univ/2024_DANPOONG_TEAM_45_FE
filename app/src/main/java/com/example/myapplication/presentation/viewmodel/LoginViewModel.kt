d172c4630c6ac5686b8695622f6add3e8c32019e
jiwon <pjw99032986@gmail.com>
2024-11-22 23:23:18 +0900

    REFACTOR(GameActivity.kt) : 로그인 api 추가
    유저 api 추가

--- app/src/main/java/com/example/myapplication/presentation/viewmodel/LoginViewModel.kt
+++ app/src/main/java/com/example/myapplication/presentation/viewmodel/LoginViewModel.kt
@@ -4,7 +4,10 @@ import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.myapplication.data.repository.remote.request.login.LogInKakaoDto
+import com.example.myapplication.data.repository.remote.response.BaseResponse
 import com.example.myapplication.data.repository.remote.response.login.LogInKakaoResponse
+import com.example.myapplication.domain.usecase.login.CheckTrainingUseCase
+import com.example.myapplication.domain.usecase.login.GetCompleteTrainingUseCase
 import com.example.myapplication.domain.usecase.login.PostKakaoLoginUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -14,16 +17,48 @@ import javax.inject.Inject
 
 @HiltViewModel
 class LoginViewModel @Inject constructor(
-    private val postKakaoLoginUseCase: PostKakaoLoginUseCase
+    private val postKakaoLoginUseCase: PostKakaoLoginUseCase,
+    private val getCompleteTrainingUseCase: GetCompleteTrainingUseCase,
+    private val getTrainingUseCase: CheckTrainingUseCase
 ) : ViewModel() {
     private val _kakaoLogin = MutableStateFlow(LogInKakaoResponse())
     val kakaoLogin: StateFlow<LogInKakaoResponse> = _kakaoLogin
+
+    private val _training = MutableStateFlow(BaseResponse<Boolean>())
+    val training: StateFlow<BaseResponse<Boolean>> = _training
+
+    private val _completeTraining = MutableStateFlow(BaseResponse<Any>())
+    val completeTraining: StateFlow<BaseResponse<Any>> = _completeTraining
 
     fun postKakaoLogin(logInKakaoDto: LogInKakaoDto) {
         viewModelScope.launch {
             try {
                 postKakaoLoginUseCase(logInKakaoDto).collect {
                     _kakaoLogin.value = it
+                }
+            } catch (e: Exception) {
+                Log.e("실패", "postKakaoLogin")
+            }
+        }
+    }
+
+    fun getTraining() {
+        viewModelScope.launch {
+            try {
+                getTrainingUseCase().collect {
+                    _training.value = it
+                }
+            } catch (e: Exception) {
+                Log.e("실패", "postKakaoLogin")
+            }
+        }
+    }
+
+    fun getCompleteTraining() {
+        viewModelScope.launch {
+            try {
+                getCompleteTrainingUseCase().collect {
+                    _completeTraining.value = it
                 }
             } catch (e: Exception) {
                 Log.e("실패", "postKakaoLogin")
