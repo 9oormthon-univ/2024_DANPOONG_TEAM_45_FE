7f62e3b3b5a71d9ce28a82c861d340b0c263c545
jiwon <pjw99032986@gmail.com>
2024-11-23 02:07:49 +0900

    FEAT(chapterClear.kt) : 빈 api 추가
    빈 api 추가

--- app/src/main/java/com/example/myapplication/data/di/ApiModule.kt
+++ app/src/main/java/com/example/myapplication/data/di/ApiModule.kt
@@ -1,11 +1,13 @@
 package com.example.myapplication.data.di
 
 import com.example.myapplication.data.repository.remote.api.ChapterApi
+import com.example.myapplication.data.repository.remote.api.ChapterClearedApi
 import com.example.myapplication.data.repository.remote.api.CharacterApi
 import com.example.myapplication.data.repository.remote.api.DifficultyApi
 import com.example.myapplication.data.repository.remote.api.HomeApi
 import com.example.myapplication.data.repository.remote.api.LoginApi
 import com.example.myapplication.data.repository.remote.api.QuizApi
+import com.example.myapplication.data.repository.remote.api.QuizClearedApi
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -51,5 +53,17 @@ object ApiModule {
     fun provideDifficultyServer(
         @MainRetrofit retrofit: Retrofit
     ): DifficultyApi = retrofit.create(DifficultyApi::class.java)
+
+    @Provides
+    @Singleton
+    fun provideChapterClearServer(
+        @MainRetrofit retrofit: Retrofit
+    ): ChapterClearedApi = retrofit.create(ChapterClearedApi::class.java)
+
+    @Provides
+    @Singleton
+    fun provideQuizClearServer(
+        @MainRetrofit retrofit: Retrofit
+    ): QuizClearedApi = retrofit.create(QuizClearedApi::class.java)
 
 }
