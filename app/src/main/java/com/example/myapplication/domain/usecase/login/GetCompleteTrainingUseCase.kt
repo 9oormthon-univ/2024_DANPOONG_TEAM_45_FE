aebcee45ed2c2380bfc3e4fa5711ba09573fe734
jiwon <pjw99032986@gmail.com>
2024-11-22 23:22:54 +0900

    REFACTOR(GameActivity.kt) : 점수 api 추가
    점수 상승 api 추가

--- app/src/main/java/com/example/myapplication/domain/usecase/login/GetCompleteTrainingUseCase.kt
+++ app/src/main/java/com/example/myapplication/domain/usecase/login/GetCompleteTrainingUseCase.kt
@@ -1,6 +1,7 @@
 package com.example.myapplication.domain.usecase.login
 
 import com.example.myapplication.data.repository.remote.request.login.LogInKakaoDto
+import com.example.myapplication.data.repository.remote.response.BaseResponse
 import com.example.myapplication.data.repository.remote.response.login.LogInKakaoResponse
 import com.example.myapplication.domain.repository.login.LoginRepository
 import kotlinx.coroutines.flow.Flow
@@ -10,5 +11,5 @@ class GetCompleteTrainingUseCase @Inject constructor(
     private val loginRepository: LoginRepository
 ) {
     suspend operator fun invoke(
-    ): Flow<Any> = loginRepository.getCompleteTraining()
+    ): Flow<BaseResponse<Any>> = loginRepository.getCompleteTraining()
 }
