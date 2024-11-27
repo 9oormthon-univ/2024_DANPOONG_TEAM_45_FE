f99b3d82c4d330dac375df3ba42b7eb4ae69fe2a
jiwon <pjw99032986@gmail.com>
2024-11-27 18:20:08 +0900

    REFACTOR(QuestChapterAdapter.kt) : 챕터 어댑터 보상 완료 API
    보상 완료 클릭 시 서버로 전송하는 api 추가

--- app/src/main/java/com/example/myapplication/domain/usecase/chapter/ModifyChapterUseCase.kt
+++ app/src/main/java/com/example/myapplication/domain/usecase/chapter/ModifyChapterUseCase.kt
@@ -1,6 +1,7 @@
 package com.example.myapplication.domain.usecase.chapter
 
 import com.example.myapplication.data.repository.remote.request.chapter.RegisterChapterDto
+import com.example.myapplication.data.repository.remote.response.BaseResponse
 import com.example.myapplication.domain.repository.chapter.ChapterRepository
 import kotlinx.coroutines.flow.Flow
 import okhttp3.ResponseBody
@@ -10,6 +11,6 @@ import javax.inject.Inject
 class ModifyChapterUseCase @Inject constructor(
     private val chapterRepository: ChapterRepository
 ) {
-    suspend operator fun invoke(registerChapterDto: RegisterChapterDto
-    ): Flow<Response<ResponseBody>> = chapterRepository.modifyChapter(registerChapterDto)
+    suspend operator fun invoke(chapter_id : String, registerChapterDto: RegisterChapterDto
+    ): Flow<BaseResponse<Any>> = chapterRepository.modifyChapter(chapter_id,registerChapterDto)
 }
