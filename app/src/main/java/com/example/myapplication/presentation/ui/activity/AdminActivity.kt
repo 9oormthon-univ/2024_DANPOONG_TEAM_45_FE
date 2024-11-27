45bb2f6b74802c3b072fd8a85d6fad01a4cb7a1f
jiwon <pjw99032986@gmail.com>
2024-11-27 18:15:40 +0900

    REFACTOR(Friends) : 관리자 페이지 추가
    관리자 페이지 추가

---
+++ app/src/main/java/com/example/myapplication/presentation/ui/activity/AdminActivity.kt
@@ -0,0 +1,67 @@
+package com.example.myapplication.presentation.ui.activity
+
+import androidx.lifecycle.ViewModelProvider
+import com.example.myapplication.R
+import com.example.myapplication.data.repository.remote.request.chapter.RegisterChapterDto
+import com.example.myapplication.data.repository.remote.request.quiz.QuizDto
+import com.example.myapplication.databinding.ActivityAdminBinding
+import com.example.myapplication.presentation.base.BaseActivity
+import com.example.myapplication.presentation.viewmodel.ChapterViewModel
+import com.example.myapplication.presentation.viewmodel.DifficultyViewModel
+import com.example.myapplication.presentation.viewmodel.QuizViewModel
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class AdminActivity : BaseActivity<ActivityAdminBinding>(R.layout.activity_admin) {
+    private lateinit var difficultyViewModel: DifficultyViewModel
+    private lateinit var chapterViewModel: ChapterViewModel
+    private lateinit var quizViewModel: QuizViewModel
+
+    override fun setLayout() {
+        initViewModel()
+        setOnClickBtn()
+    }
+
+    private fun initViewModel() {
+        difficultyViewModel = ViewModelProvider(this)[DifficultyViewModel::class.java]
+        chapterViewModel = ViewModelProvider(this)[ChapterViewModel::class.java]
+        quizViewModel = ViewModelProvider(this)[QuizViewModel::class.java]
+    }
+
+    private fun setOnClickBtn() {
+        with(binding) {
+            quizBt.setOnClickListener {
+                quizViewModel.postCreateQuiz(
+//                    QuizDto(
+//                        listOf(),
+//                        listOf(),
+//                        "NORMAL",
+//                        "1단계 : 기초 훈련하기",
+//                        "초보 모험가를 위한 기초 훈련!",
+//                        chapterEt.text.toString().toInt(),
+//                        1,
+//                        ""
+//                    )
+
+                    QuizDto(
+                        listOf(),
+                        listOf(),
+                        "NORMAL",
+                        "2단계 : 모험 준비하기",
+                        "본격적으로 모험을 준비해봐요!",
+                        chapterEt.text.toString().toInt(),
+                        1,
+                        ""
+                    )
+
+                )
+            }
+            difficultyBt.setOnClickListener {
+                difficultyViewModel.postCreateDifficulty("1")
+            }
+            chapterBt.setOnClickListener {
+                chapterViewModel.postCreateChapter(binding.chapterEt.text.toString())
+            }
+        }
+    }
+}
