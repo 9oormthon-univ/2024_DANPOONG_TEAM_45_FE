067d73f113e0cfe62f11868d2af52f8c2ddd77e6
jiwon <pjw99032986@gmail.com>
2024-12-02 03:59:21 +0900

    FEAT(MULTI-MODULE) : 빌드 로직, 버전 카탈로그
    버전 카탈로그 경로 지정 및 빌드 로직, 컨벤션 모듈 포함

---
+++ build-logic/settings.gradle.kts
@@ -0,0 +1,16 @@
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven { url = java.net.URI("https://devrepo.kakao.com/nexus/content/groups/public/") }
+    }
+    versionCatalogs {
+        create("libs") {
+            from(files("../gradle/libs.versions.toml"))
+        }
+    }
+}
+
+rootProject.name = "build-logic"
+include(":convention")
