aee4508fe0c7b46e2e47ee515e05c8050b0356a4
jiwon <pjw99032986@gmail.com>
2024-12-02 04:02:44 +0900

    FEAT(MULTI-MODULE) : 멀티모듈
    기본 코틀린 공통 build 환경 분리

---
+++ build-logic/convention/src/main/java/com/multi/module/convention/Kotlin.kt
@@ -0,0 +1,46 @@
+package com.multi.module.convention
+
+import com.android.build.api.dsl.CommonExtension
+import org.gradle.api.JavaVersion
+import org.gradle.api.Project
+import org.gradle.api.artifacts.VersionCatalog
+import org.gradle.api.artifacts.VersionCatalogsExtension
+import org.gradle.kotlin.dsl.dependencies
+import org.gradle.kotlin.dsl.getByType
+import org.gradle.kotlin.dsl.withType
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+internal fun Project.configureKotlinAndroid(
+    commonExtension: CommonExtension<*, *, *, *, *, *>
+) {
+    commonExtension.apply {
+        compileSdk = libs.findVersion("projectCompileSdkVersion").get().toString().toInt()
+
+        defaultConfig {
+            minSdk = libs.findVersion("projectMinSdkVersion").get().toString().toInt()
+        }
+
+        compileOptions {
+            isCoreLibraryDesugaringEnabled = true
+            sourceCompatibility = JavaVersion.VERSION_1_8
+            targetCompatibility = JavaVersion.VERSION_1_8
+        }
+    }
+
+    configureKotlin()
+
+    dependencies {
+        "coreLibraryDesugaring"(libs.findLibrary("desugar-jdk-libs").get())
+    }
+}
+
+private fun Project.configureKotlin() {
+    tasks.withType<KotlinCompile>().configureEach {
+        kotlinOptions {
+            jvmTarget = "1.8"
+        }
+    }
+}
+
+val Project.libs: VersionCatalog
+    get() = extensions.getByType<VersionCatalogsExtension>().named("libs")
