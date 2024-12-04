6b8534ee546109261fea2dd5da291d49cc4360f3
Eunchae Cho <choeunchae0828@gmail.com>
2024-12-05 02:38:49 +0900

    FEAT(TutorialActivity) : 튜토리얼 1번 fragment 추가

---
+++ app/src/main/java/com/example/myapplication/presentation/ui/activity/TutorialActivity.kt
@@ -0,0 +1,78 @@
+package com.example.myapplication.presentation.ui.activity
+
+import androidx.navigation.NavController
+import androidx.navigation.NavOptions
+import androidx.navigation.fragment.NavHostFragment
+import com.example.myapplication.R
+import com.example.myapplication.databinding.ActivityTutorialBinding
+import com.example.myapplication.presentation.base.BaseActivity
+import com.example.myapplication.presentation.ui.fragment.quest.DialogClickListener
+
+class TutorialActivity: BaseActivity<ActivityTutorialBinding>(R.layout.activity_tutorial),
+    DialogClickListener {
+
+    private lateinit var navController: NavController
+    private var buttonPosition = 1
+    private var levelCorrect = false
+
+    override fun setLayout() {
+        setNavController()
+        nextFragment()
+    }
+
+    //네비게이션 컨트롤러 세팅
+    private fun setNavController() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(binding.activityTutorialFcv.id) as NavHostFragment
+        navController = navHostFragment.navController
+    }
+
+    private fun nextFragmentWithIndex() {
+        when (buttonPosition) {
+//            1 -> {
+//                navController.navigate(
+//                    R.id.quiz2Fragment, null,
+//                    NavOptions.Builder()
+//                        .setPopUpTo(R.id.quiz1Fragment, true)  // 시작 프래그먼트 제거
+//                        .setLaunchSingleTop(true)
+//                        .build()
+//                )
+//            }
+//
+//            2 -> {
+//                navController.navigate(
+//                    R.id.quiz3Fragment, null,
+//                    NavOptions.Builder()
+//                        .setPopUpTo(R.id.quiz2Fragment, true)  // 이전 프래그먼트 제거
+//                        .setLaunchSingleTop(true)
+//                        .build()
+//                )
+//            }
+//
+//            3 -> {
+//                quizViewModel.postQuizClear(1)
+//            }
+        }
+    }
+
+    //버튼 이동
+    private fun nextFragment() {
+        binding.activityTutorialFcv.setOnClickListener {
+            if (it.isSelected) {
+                nextFragmentWithIndex()
+            }
+        }
+    }
+
+    fun setReplaceLevelState(levelState: Boolean) {
+        levelCorrect = levelState
+    }
+
+    override fun onClickNext() {
+        nextFragmentWithIndex()
+    }
+
+    override fun onClickStop() {
+        finish()
+    }
+}
