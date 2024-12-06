0fc440293100ba3d7ed25994b90d6dfce634c77a
jiwon <pjw99032986@gmail.com>
2024-12-06 17:33:34 +0900

    FEAT(MULTI-MODULE) : 공용 라이브러리 플러그인 정의
    공용 라이브러리 플러그인 정의

--- build-logic/convention/src/main/kotlin/convention/Kotlin.kt
+++ build-logic/convention/src/main/kotlin/convention/Kotlin.kt
@@ -5,6 +5,7 @@ import org.gradle.api.JavaVersion
 import org.gradle.api.Project
 import org.gradle.api.artifacts.VersionCatalog
 import org.gradle.api.artifacts.VersionCatalogsExtension
+import org.gradle.api.artifacts.dsl.DependencyHandler
 import org.gradle.kotlin.dsl.dependencies
 import org.gradle.kotlin.dsl.getByType
 import org.gradle.kotlin.dsl.withType
@@ -42,5 +43,92 @@ private fun Project.configureKotlin() {
     }
 }
 
+fun Project.configureDependencies() {
+
+    val imp = "implementation"
+    val ksp = "ksp"
+    dependencies {
+        // Hilt Dependencies (KSP)
+        add(imp, "com.google.dagger:hilt-android:2.52")
+        add(ksp, "com.google.dagger:hilt-android-compiler:2.52")
+
+        // Room Dependencies (KSP)
+        add(imp, "androidx.room:room-runtime:2.6.1")
+        add(ksp, "androidx.room:room-compiler:2.6.1")
+        add(imp, "androidx.room:room-ktx:2.6.1")
+
+        //data store
+        add(imp, "androidx.datastore:datastore-preferences:1.1.1")
+
+        // Retrofit
+        add(imp, "com.squareup.retrofit2:retrofit:2.11.0")
+        add(imp, "com.squareup.retrofit2:converter-gson:2.11.0")
+
+        // OkHttp
+        add(imp, "com.squareup.okhttp3:okhttp:4.12.0")
+        add(imp, "com.squareup.okhttp3:logging-interceptor:4.12.0")
+        add(imp, "com.squareup.okhttp3:okhttp-urlconnection:4.12.0")
+        add(imp, platform("com.squareup.okhttp3:okhttp-bom:4.12.0")) // BOM 사용
+
+        // Gson
+        add(imp, "com.google.code.gson:gson:2.11.0")
+    }
+}
+
+fun Project.configureCommonDependencies() {
+    val imp = "implementation"
+    dependencies {
+        add("implementation", libs.findLibrary("androidx.core.ktx").get())
+        add("implementation", libs.findLibrary("androidx.appcompat").get())
+        add("implementation", libs.findLibrary("material").get())
+        add("implementation", libs.findLibrary("junit").get())
+        add("androidTestImplementation", libs.findLibrary("androidx.junit").get())
+        add("androidTestImplementation", libs.findLibrary("androidx.espresso.core").get())
+    }
+}
+
+fun Project.configureLibraryDependencies() {
+    dependencies {
+        // Navigation
+        add("implementation", libs.findLibrary("androidx.navigation.fragment.ktx").get())
+        add("implementation", libs.findLibrary("androidx.navigation.ui.ktx").get())
+
+        // Glide
+        add("implementation", libs.findLibrary("glide").get())
+        add("implementation", libs.findLibrary("androidx.paging.common.android").get())
+        add("ksp", libs.findLibrary("compiler").get())
+
+        // ViewPager2
+        add("implementation", libs.findLibrary("androidx.viewpager2").get())
+
+        // Indicator
+        add("implementation", libs.findLibrary("relex.circleindicator").get())
+
+        // Kakao SDK
+        add("implementation", libs.findLibrary("com.kakao.sdk.v2.all").get())
+
+        // Splash Screen
+        add("implementation", libs.findLibrary("androidx.core.splashscreen").get())
+
+        // Activity & ConstraintLayout
+        add("implementation", libs.findLibrary("androidx.activity").get())
+        add("implementation", libs.findLibrary("androidx.constraintlayout").get())
+
+        // Drag and Drop
+        add("implementation", libs.findLibrary("androidx.draganddrop").get())
+
+        // Coroutines
+        add("implementation", libs.findLibrary("kotlinx.coroutines.android").get())
+
+        // Lottie
+        add("implementation", libs.findLibrary("lottie").get())
+
+        // Profile Installer
+        add("implementation", libs.findLibrary("androidx.profileinstaller").get())
+    }
+}
+
+
 val Project.libs: VersionCatalog
     get() = extensions.getByType<VersionCatalogsExtension>().named("libs")
+
