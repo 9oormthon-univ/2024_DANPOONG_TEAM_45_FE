4d7e00349a3da0808a3bb7d208a8783d01dba038
jiwon <pjw99032986@gmail.com>
2024-12-06 17:32:02 +0900

    FEAT(MULTI-MODULE) : 공용 라이브러리 플러그인 정의
    공용 라이브러리 플러그인 정의

--- build-logic/convention/src/main/kotlin/AndroidLibraryConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/AndroidLibraryConventionPlugin.kt
@@ -1,11 +1,13 @@
 import com.android.build.api.dsl.LibraryExtension
 import convention.ExtensionType
 import convention.configureBuildTypes
+import convention.configureCommonDependencies
+import convention.configureDependencies
 import convention.configureKotlinAndroid
+import convention.libs
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.configure
-import org.gradle.kotlin.dsl.dependencies
 
 class AndroidLibraryConventionPlugin : Plugin<Project> {
     override fun apply(target: Project) {
@@ -19,46 +21,22 @@ class AndroidLibraryConventionPlugin : Plugin<Project> {
 
             extensions.configure<LibraryExtension> {
                 configureKotlinAndroid(this)
+
+                configureCommonDependencies()
+
                 configureDependencies()
+
                 configureBuildTypes(
                     commonExtension = this,
                     extensionType = ExtensionType.LIBRARY
                 )
 
                 defaultConfig {
-                    testInstrumentationRunner = "androidx.text.runner.AndroidJUnitRunner"
-                    consumerProguardFiles("consumer-rules.pro")
+                    testInstrumentationRunner = libs.findVersion("testInstrumentationRunner").get().toString()
                 }
             }
         }
     }
 }
 
-fun Project.configureDependencies() {
-    dependencies {
-        // Hilt Dependencies (KSP)
-        add("implementation", "com.google.dagger:hilt-android:2.52")
-        add("ksp", "com.google.dagger:hilt-android-compiler:2.52")
 
-        // Room Dependencies (KSP)
-        add("implementation", "androidx.room:room-runtime:2.6.1")
-        add("ksp", "androidx.room:room-compiler:2.6.1")
-        add("implementation", "androidx.room:room-ktx:2.6.1")
-
-        //data store
-        add("implementation", "androidx.datastore:datastore-preferences:1.1.1")
-
-        // Retrofit
-        add("implementation", "com.squareup.retrofit2:retrofit:2.11.0")
-        add("implementation", "com.squareup.retrofit2:converter-gson:2.11.0")
-
-        // OkHttp
-        add("implementation", "com.squareup.okhttp3:okhttp:4.12.0")
-        add("implementation", "com.squareup.okhttp3:logging-interceptor:4.12.0")
-        add("implementation", "com.squareup.okhttp3:okhttp-urlconnection:4.12.0")
-        add("implementation", platform("com.squareup.okhttp3:okhttp-bom:4.12.0")) // BOM 사용
-
-        // Gson
-        add("implementation", "com.google.code.gson:gson:2.11.0")
-    }
-}
