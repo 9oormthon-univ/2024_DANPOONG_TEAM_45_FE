778c7317c81b4be69aaabf07b25996a0ee76b0db
jiwon <pjw99032986@gmail.com>
2024-12-06 17:31:28 +0900

    FEAT(MULTI-MODULE, RELEASE) : 멀티 모듈, 릴리즈 서명 추가
    릴리즈 서명, jks 키, 모듈 추가

--- build-logic/convention/src/main/kotlin/AndroidApplicationConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/AndroidApplicationConventionPlugin.kt
@@ -1,11 +1,15 @@
 import com.android.build.api.dsl.ApplicationExtension
 import convention.ExtensionType
 import convention.configureBuildTypes
+import convention.configureCommonDependencies
+import convention.configureDependencies
 import convention.configureKotlinAndroid
+import convention.configureLibraryDependencies
 import convention.libs
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.configure
+import org.gradle.kotlin.dsl.dependencies
 
 class AndroidApplicationConventionPlugin : Plugin<Project> {
     override fun apply(target: Project) {
@@ -28,8 +32,31 @@ class AndroidApplicationConventionPlugin : Plugin<Project> {
                     testInstrumentationRunner = libs.findVersion("testInstrumentationRunner").get().toString()
                 }
 
+                signingConfigs{
+                    create("release"){
+                        storeFile = file("/keystore/moomoo.jks")  // 키스토어 파일 경로
+                        storePassword = "990329"  // 키스토어 비밀번호
+                        keyAlias = "moomoo"  // 키 별칭
+                        keyPassword = "990329"  // 키 비밀번호
+                    }
+                }
+
+                buildTypes {
+                    getByName("release") {
+                        isMinifyEnabled = true  // 난독화 활성화
+                        isShrinkResources = true  // 리소스 축소
+                        signingConfig = signingConfigs.getByName("release")  // 릴리즈 서명 구성 연결
+                    }
+                }
+
                 configureKotlinAndroid(this)
+
+                configureCommonDependencies()
+
                 configureDependencies()
+
+                configureLibraryDependencies()
+
                 configureBuildTypes(
                     commonExtension = this,
                     extensionType = ExtensionType.APPLICATION
@@ -37,4 +64,8 @@ class AndroidApplicationConventionPlugin : Plugin<Project> {
             }
         }
     }
+
 }
+
+
+
